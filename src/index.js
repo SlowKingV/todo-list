b0d892155d418189df89f387bd6a3524f8d137d2
Diego Luna <slowkingvii@gmail.com>
2022-07-13 12:22:51 -0500

    Add 'moveTaskToPosition' method to TaskList

--- src/index.js
+++ src/index.js
@@ -10,10 +10,19 @@ import TaskList from './modules/taskList.js';
 // On page load populate task list
 window.onload = () => {
   const listContainer = document.getElementById('list');// <ul> element to put the <li>s
-  const tasklist = new TaskList();
+  const tasklist = new TaskList(listContainer);
+
+  // Example task operations
   tasklist.add('Task 0');
   tasklist.add('Task 1');
   tasklist.add('Task 2');
   tasklist.add('Task 3');
-  tasklist.insertItems(listContainer);
+  tasklist.remove(2);
+  tasklist.add('Task 5');
+  // Moves task on the index 2 ('Task 3') to index 1 (before 'Task 1')
+  tasklist.moveTaskToPosition(2, 1);
+  // Moves last task ('Task 5') to the beginning (before 'Task 0')
+  tasklist.moveTaskToPosition(tasklist.list.length - 1, 0);
+
+  tasklist.updateListHTML();
 };
--- src/modules/taskList.js
+++ src/modules/taskList.js
@@ -1,7 +1,8 @@
 import Task from './task.js';
 
 export default class {
-  constructor(list = []) {
+  constructor(container, list = []) {
+    this.container = container;
     this.list = list;
   }
 
@@ -11,20 +12,26 @@ export default class {
 
   remove(index) {
     this.list.splice(index, 1);
-    this.refreshItemsIndex();
+    this.#updateTasksIndex();
   }
 
-  refreshItemsIndex() {
-    this.list.forEach((item, index) => { item.index = index; });
+  moveTaskToPosition(taskIndex, newIndex) {
+    const task = this.list.splice(taskIndex, 1)[0];
+    this.list.splice(newIndex, 0, task);
+    this.#updateTasksIndex();
   }
 
-  insertItems(element) {
+  updateListHTML() {
     const items = [];
     const rule = document.createElement('li');
     rule.appendChild(document.createElement('hr'));
 
     this.list.forEach((task) => items.push(task.element, rule.cloneNode(true)));
 
-    element.replaceChildren(...items);
+    this.container.replaceChildren(...items);
+  }
+
+  #updateTasksIndex() {
+    this.list.forEach((item, index) => { item.index = index; });
   }
 }
