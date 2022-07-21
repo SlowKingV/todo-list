6fa93f966f749c6e0fc1f091e0c62a2608e9f51b
Diego Luna <slowkingvii@gmail.com>
2022-07-21 11:19:42 -0500

    Move handler functions to 'DOMFunctions.js'

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './style.css';
 
 // Modules
 import TaskList from './modules/taskList.js';
+import { tasklistAdd, clearAll, loadData } from './modules/DOMFunctions';
 
 // Declare constants for DOM Elements
 const addTaskForm = document.getElementById('add-form');
@@ -13,62 +14,19 @@ const tasklist = new TaskList(document.getElementById('list'));
 const textInput = document.getElementById('text-input');
 const clearButton = document.getElementById('clear-btn');
 
-// Save into LocalStorage
-const updateLocalStorage = () => {
-  localStorage.datalist = JSON.stringify(tasklist.exportListValues());
-};
-
-// Set Task input handlers
-const setTaskHandlers = (task) => {
-  // Set Delete button handler
-  task.element.DELETE_BUTTON.addEventListener('click', () => {
-    tasklist.remove(task.index);
-    tasklist.updateListHTML();
-    updateLocalStorage();
-  });
-
-  // Set Text input handler
-  task.element.TEXT_INPUT.addEventListener('change', () => {
-    tasklist.edit(task.index, task.element.TEXT_INPUT.value);
-    tasklist.updateListHTML();
-    updateLocalStorage();
-  });
-
-  // Set Checkbox handler
-  task.element.CHECKBOX.addEventListener('change', () => {
-    tasklist.setStatus(task.index, task.element.CHECKBOX.checked);
-    updateLocalStorage();
-  });
-};
-
 // Set Handler for 'Add' text input
 addTaskForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  const task = tasklist.add(textInput.value);
-
-  setTaskHandlers(task);
-
-  tasklist.updateListHTML();
-  updateLocalStorage();
+  tasklistAdd(tasklist, textInput.value);
   textInput.value = '';
 });
 
 // Set Handler for 'Clear All Completed' button
 clearButton.addEventListener('click', () => {
-  tasklist.clearAllCompleted();
-  tasklist.updateListHTML();
-  updateLocalStorage();
+  clearAll(tasklist);
 });
 
 // On page load populate task list
 window.onload = () => {
-  if (localStorage.datalist && localStorage.datalist.length > 0) {
-    tasklist.regenerateValues(JSON.parse(localStorage.datalist));
-  } else updateLocalStorage();
-
-  tasklist.updateListHTML();
-
-  tasklist.list.forEach((task) => {
-    setTaskHandlers(task);
-  });
+  loadData(tasklist);
 };
---
+++ src/modules/DOMFunctions.js
@@ -0,0 +1,56 @@
+// Save into LocalStorage
+const updateLocalStorage = (tasklist) => {
+  localStorage.datalist = JSON.stringify(tasklist.exportListValues());
+};
+
+// Set Task input handlers
+const setTaskHandlers = (task, tasklist) => {
+  // Set Delete button handler
+  task.element.DELETE_BUTTON.addEventListener('click', () => {
+    tasklist.remove(task.index);
+    tasklist.updateListHTML();
+    updateLocalStorage(tasklist);
+  });
+
+  // Set Text input handler
+  task.element.TEXT_INPUT.addEventListener('change', () => {
+    tasklist.edit(task.index, task.element.TEXT_INPUT.value);
+    tasklist.updateListHTML();
+    updateLocalStorage(tasklist);
+  });
+
+  // Set Checkbox handler
+  task.element.CHECKBOX.addEventListener('change', () => {
+    tasklist.setStatus(task.index, task.element.CHECKBOX.checked);
+    updateLocalStorage(tasklist);
+  });
+};
+
+// Set Handler for 'Add' text input
+export const tasklistAdd = (tasklist, value) => {
+  const task = tasklist.add(value);
+
+  setTaskHandlers(task, tasklist);
+
+  tasklist.updateListHTML();
+  updateLocalStorage(tasklist);
+}
+
+// Set Handler for 'Clear All Completed' button
+export const clearAll = (tasklist) => {
+  tasklist.clearAllCompleted();
+  tasklist.updateListHTML();
+  updateLocalStorage(tasklist);
+}
+
+export const loadData = (tasklist) => {
+  if (localStorage.datalist && localStorage.datalist.length > 0) {
+    tasklist.regenerateValues(JSON.parse(localStorage.datalist));
+  } else updateLocalStorage(tasklist);
+
+  tasklist.updateListHTML();
+
+  tasklist.list.forEach((task) => {
+    setTaskHandlers(task, tasklist);
+  });
+}
